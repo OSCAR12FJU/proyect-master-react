6d548ad6158d7ad11fe4abb0b0dee80c192f0dae
Oscar Flores <floresquispeoscar109@gmail.com>
2024-02-28 14:37:58 -0300

    Subiendo proyecto

---
+++ proyect-vite/vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import {inico, articulo, articulos, busqueda, crear, editar} from "./componentes/layout/pages"
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+      <inicio />
+      <articulo />
+      <articulos />
+      <busqueda />
+      <crear />
+      <editar />
+
+
+
+    </div>
+  )
+} 
+
+export default App
